38337d1c77c9fc70effa61c6531a45266140e0d1
QuinnMcAuliffe <156962574+QuinnMcAuliffe@users.noreply.github.com>
2024-09-27 14:43:22 -0400

    Added more tests

--- src/phase1_scanner/ScanTest.java
+++ src/phase1_scanner/ScanTest.java
@@ -81,4 +81,82 @@ public class ScanTest {
         assertTrue(outContent.toString().contains("State: CLOSE_PARAN"));
     }
 
+    @Test
+    public void testAddOp() {
+        simulateInput("+");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: ADD_OP"));
+    }
+
+    @Test
+    public void testSubtractOp() {
+        simulateInput("-");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: SUBTRACT_OP"));
+    }
+
+    @Test
+    public void testMultiplyOp() {
+        simulateInput("*");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: MULTIPLY_OP"));
+    }
+
+    @Test
+    public void testDivideOp() {
+        simulateInput("/");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: DIVIDE_OP"));
+    }
+
+    @Test
+    public void testEqualOp() {
+        simulateInput("=");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: ASSIGN_OP"));
+    }
+
+    @Test
+    public void testNotEqualOp() {
+        simulateInput("!=");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: NOT_EQUAL_OP"));
+    }
+
+    @Test
+    public void testLessThanOp() {
+        simulateInput("<");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: LT_OP"));
+    }
+
+    @Test
+    public void testLessThanOrEqualOp() {
+        simulateInput("<=");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: LT_ET_OP"));
+    }
+
+    @Test
+    public void testGreaterThanOp() {
+        simulateInput(">");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: GT_OP"));
+    }
+
+    @Test
+    public void testGreaterThanOrEqualOp() {
+        simulateInput(">=");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: GT_ET_OP"));
+    }
+
+    @Test
+    public void testExclamationOp() {
+        simulateInput("!");
+        scanner.main(new String[0]);
+        assertTrue(outContent.toString().contains("State: EXCLAMATION_STATE"));
+    }
+    
+
 }
