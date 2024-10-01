d800e25a32372aa5a74e2b7293b3b31f52360703
Greyson Meares <gcm00010@mix.wvu.edu>
2024-09-30 23:09:26 -0400

    Minor adjustments to comments

--- src/phase1_scanner/Scan.java
+++ src/phase1_scanner/Scan.java
@@ -15,7 +15,6 @@ import java.util.Scanner;
  * 9/24/2024
  * 
  * To Note: 
- *    - Spaces are needed in between each token to identify the token
  *    - States F, FL, FLO, FLOA, I, IN, W, WH, WHI, WHIL, E, EL, ELS, ELSEI are assumed to be IDENTIFIERS
  * 
  ************************************************************/
@@ -25,13 +24,15 @@ public class Scan {
         //Adding class for file: javac src/phase1_scanner/*.java
         //Compile the files:  javac -d bin src/phase1_scanner/StateTable.java src/phase1_scanner/Scan.java
         //Run the program:  java -cp bin phase1_scanner.Scan
+
     public static void main(String[] args) {
 
+        //Create StateTable object
         StateTable info = new StateTable();
         int[][] state_table = info.st_table;
         String[] accepting_states = info.acceptingStates_string;
 
-        //initial state
+        //Initial state
         int state = 0;
 
         //Gather input from user
@@ -44,7 +45,7 @@ public class Scan {
         int states[] = new int[100];
         String states_string[] = new String[100];
 
-        //current character
+        //Current character
         int inp;
 
         int j = 0;
@@ -52,9 +53,10 @@ public class Scan {
         //Previous state
         int prevState = 0;
 
+        //Traverse the state table to tokenize the input
         for(int i = 0; i < input.length() ; i++){
             StringBuilder token = new StringBuilder();
-            //read character from input stream
+            //Read character from input stream
             while(i<input.length() && (inp=input.charAt(i) - ' ') > 0){
                 state = state_table[state][inp];
 
@@ -77,7 +79,7 @@ public class Scan {
             //If a character is not covered by the transition table
             if(state == info.INVALID && token.length() == 0){
                 //i+1 is necessary to counteract the i-- before it got here
-                //print final states
+                //Print final states
                 print_states(states, accepting_states, states_string);
                 System.out.println(input.charAt(i+1) + " is not a valid character.");
                 System.exit(-1);
@@ -93,7 +95,7 @@ public class Scan {
             states[j++] = prevState;
 
 
-            //reset state
+            //Reset state for next token
             state = 0;
         }
 
@@ -101,6 +103,7 @@ public class Scan {
         print_states(states, accepting_states, states_string);
     };
 
+    // Method to print final states
     static void print_states(int[] states, String[] accepting_states, String[] states_string){
         System.out.println("\nFinal States: ");
         for(int i = 0; states[i] != 0 || states_string[i] != null; i++){
