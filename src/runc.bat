8a35a71ee6e267624408e460f95aee6107b5b705
Greyson Meares <greysonmeares@gmail.com>
2024-12-19 12:09:51 -0500

    Added run.bat and runc.bat to parse then generate. runc compiles the generater, parse, runfront, and runback java files before running our program. run.bat does not compile the java files and just runs our program. Use runc is changes have been made.

--- src/runc.bat
+++ src/runc.bat
@@ -1,6 +1,9 @@
 @echo off
 
 javac .\phase3_generator\Generator.java
+javac .\phase2_parser\Parse.java
 javac .\RunBack.java
+javac .\RunFront.java
 
-java RunBack %*
+java RunFront %1 intermediate.atoms %3 
+java RunBack intermediate.atoms %2 %3
